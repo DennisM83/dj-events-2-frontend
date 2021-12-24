f5e4af3b13d79541d767ade642ae005c5f74c2d6
Dennis Wenrich <dennisfinley@gmail.com>
2021-12-24 13:02:49 -0500

    added api to events page

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,7 @@
 import Layout from '@/components/Layout'
 import EventItem from '@/components/EventItem'
 import { API_URL } from '@/config/index'
-export default function HomePage({ events }) {
-  
-  console.log(events)
+export default function HomePage( {events} ) {
   return (
     <Layout>
       <h1>Upcoming Events</h1>
@@ -18,7 +16,7 @@ export default function HomePage({ events }) {
 export async function getStaticProps() {
   const res = await fetch(`${API_URL}/api/events`)
   const events = await res.json()
-
+  console.log(events)
   return {
     props: events,
     revalidate: 1
