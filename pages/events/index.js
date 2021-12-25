03cc8247aceccc18eb1a3365879cf98f2984324d
Dennis Wenrich <dennisfinley@gmail.com>
2021-12-25 11:28:38 -0500

    added events dynamically

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,27 +1,24 @@
-import Layout from '@/components/Layout'
-import EventItem from '@/components/EventItem'
-import { API_URL } from '@/config/index'
+import Layout from "@/components/Layout";
+import EventItem from "@/components/EventItem";
+import { API_URL } from "@/config/index";
 export default function EventsPage({ events }) {
-  
-  console.log(events)
   return (
     <Layout>
       <h1>Upcoming Events</h1>
       {events.length === 0 && <h3>No Events To Show</h3>}
-      {events.map(evt => (
-        <EventItem key={evt.id} evt={evt}/>
+      {events.map((evt) => (
+        <EventItem key={evt.id} evt={evt} />
       ))}
     </Layout>
-  )
+  );
 }
 
 export async function getStaticProps() {
-  const res = await fetch(`${API_URL}/api/events`)
-  const events = await res.json()
+  const res = await fetch(`${API_URL}/api/events`);
+  const events = await res.json();
 
   return {
     props: events,
-    revalidate: 1
-  }
+    revalidate: 1,
+  };
 }
-
